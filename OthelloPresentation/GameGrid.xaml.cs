4e21ffb669921a2431f5e1d3e3e61f75fe995ba6
Daniel Johansson <joda20ku@student.ju.se>
2023-05-17 08:31:58 +0200

    update

--- OthelloPresentation/GameGrid.xaml.cs
+++ OthelloPresentation/GameGrid.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace OthelloPresentation
 {
@@ -20,9 +23,38 @@ namespace OthelloPresentation
     /// </summary>
     public partial class GameGrid : UserControl
     {
+        public ObservableCollection<ObservableCollection<Brush>> GameGridColor { get; set; } = new();
+
         public GameGrid()
         {
+            for (int i = 0; i < 8; i++) 
+            {
+                GameGridColor.Add(new ObservableCollection<Brush>());
+                for (int j = 0; j < 8; j++)
+                    GameGridColor[i].Add(new SolidColorBrush());
+            }
+                
             InitializeComponent();
+            
+            this.DataContext = this;
         }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Trace.WriteLine(e.GetPosition(Grid));
+            int h = (int)Grid.ActualHeight;
+            int w = (int)Grid.ActualWidth;
+            Point p = e.GetPosition(Grid);
+            int r = (int)p.Y * 8 / h;
+            int c = (int)p.X * 8 / w;
+            Trace.WriteLine("("+ c.ToString() + ","+r.ToString()+")");
+            var point = Mouse.GetPosition(sender as Canvas);
+            Trace.WriteLine("(" + point.X.ToString() + "," + point.Y.ToString() + ")");
+
+            //var t = (SolidColorBrush)GameGridColor[r][c];
+            //t.Color = Colors.Blue;
+        }
+
+
     }
 }
